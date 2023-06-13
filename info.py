fbd827809354f35bdc764221d01495fe32038a85
Gaxenclark <129574882+Gaxenclark@users.noreply.github.com>
2023-06-14 00:04:06 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,8 +1,8 @@
-API_ID       = 
-API_HASH     = ""
-BOT_TOKEN    = ""
-SESSION      = ""
-DATABASE_URI = ""
-LOG_CHANNEL  = 
-ADMIN        = 
-CHANNEL      = "@CyNiTeBaCkUp"
+API_ID       = 25242804
+API_HASH     = "bdc317842f60220121ded0a2fcdc0727"
+BOT_TOKEN    = "6212399137:AAG59qqOLn306rydBPF9pR8w7qeEX4UozgI"
+SESSION      = "BQGBLLQAe4yuVZpffPofPoHBJGhevp0FIMBbRBaFQdTwKnH_HmUH1ELPIaT4EXpVqcDTQsniHFeBvUTx6c13bW8FgCwu8we-lD5JQqTvXI2oLaAoEv1RWyupfmcpmzKMu3yEVw-YD06zTqmQorLXYwLcK1AT09RRtk12nWjYMdcsAD_pijzbAs36oZG1j5Z61P54onLgBc5rHkFPeYEVBWHB9pPAGEzfgk5HaecpbwOaLLHCaaqRZYvT6sAKXdTIZm8Jjd1Ii9qYxyPbrixfaTryOr-h83tRxSK8j4DUu99AZQYonFDgOveoVIVtYUDW5LlPCU0My9gAfmk41kiHCCkCN-LeegAAAAFoXxEWAA"
+DATABASE_URI = "mongodb+srv://gdc846017:blackpanther00@cluster0.zpakafu.mongodb.net/?retryWrites=true&w=majority"
+LOG_CHANNEL  = -1001930500779
+ADMIN        = 2068595855
+CHANNEL      = "@StarkBackup0"
